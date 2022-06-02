dd0fdcb979fb3c6484007b2c0aaedf880bfb6a34
Eyoatam Tesfaye <teyoatam@gmail.com>
2022-06-02 02:17:44 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@ int main(void)
 {
 
 		printf("Size of a char: %zu byte(s)\n", sizeof(char));
-i
+
 			printf("Size of an int: %zu byte(s)\n", sizeof(int));
 
 				printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
