855b7d573ed039bb12c92120a2be6c3d807d85ef
Eyoatam Tesfaye <teyoatam@gmail.com>
2022-06-03 03:04:04 -0700

    print alphabet

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
    * main - Prints if number is positive, zero or negative
    * Return: Always (Success)
    */
 int main(void)
-
 {
 
 		int n;
