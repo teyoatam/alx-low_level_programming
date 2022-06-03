4e4332b0ecbccfcdc7488ee9554159451c85670f
Eyoatam Tesfaye <teyoatam@gmail.com>
2022-06-03 03:23:30 -0700

    print alphabet

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 		int n;
-
                 srand(time(0));
 		n = rand() - RAND_MAX / 2;
                 if (n > 0)
@@ -23,8 +22,5 @@ int main(void)
 		{
 			printf("%d is negative\n", n);
 		}
-
-
 		return (0);
-
 }
