1d02fab064c260116c42c7cb7234512dd7c1ea50
Eyoatam Tesfaye <teyoatam@gmail.com>
2022-06-03 19:28:20 -0700

    the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,11 +4,10 @@
 /**
   * main - Entry point
   * Description: checks on the last digit of the assigned var 
-  */
+  * Return: Always 0 (success)*/
 int main(void)
 {
 	int n, m;
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
