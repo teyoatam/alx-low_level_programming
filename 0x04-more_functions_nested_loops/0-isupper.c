3458a3c90f12007dc39f94224bd93ea7033227b5
Eyoatam Tesfaye <teyoatam@gmail.com>
2022-06-08 21:59:27 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ * Prototype: int _isupper(int c);
+ * Returns 1 if c is uppercase
+ * Returns 0 otherwise
+ */
+int _isupper(int c)
+{
+if(c>=65&&c<=90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
